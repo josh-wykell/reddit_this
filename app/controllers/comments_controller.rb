8f0c8fd222b18bd14ffffa4cec6d97992c9b6f79
Josh Wykell <jwykell@gmail.com>
2015-06-14 16:11:00 -0400

    comments working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,9 @@
 class CommentsController < ApplicationController
-<<<<<<< HEAD
-   def create
-    @post = Post.find(params[:post_id])
-    @post.comments.create(comment_params)
-    redirect_to @post, notice: "Post successful."
-=======
 
   def create
     @post = Post.find(params[:post_id])
     @post.comments.create(comment_params)
     redirect_to @post, notice: "Comment successful."
->>>>>>> f0b5eb515fd89b0b3b5e695ea755d6d467768cda
   end
 
   def destroy
@@ -18,20 +11,12 @@ class CommentsController < ApplicationController
     @comment = @post.comments.find(params[:id])
     @comment.destroy
 
-<<<<<<< HEAD
-    redirect_to @review, notice: "Post deleted."
-=======
     redirect_to @post, notice: "Comment deleted."
->>>>>>> f0b5eb515fd89b0b3b5e695ea755d6d467768cda
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:author, :body)
+    params.require(:comment).permit(:body)
   end
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> f0b5eb515fd89b0b3b5e695ea755d6d467768cda
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150614120318) do
+ActiveRecord::Schema.define(version: 20150612191107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20150614120318) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "author"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree

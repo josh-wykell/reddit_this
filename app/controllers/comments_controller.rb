f0b5eb515fd89b0b3b5e695ea755d6d467768cda
Josh Wykell <jwykell@gmail.com>
2015-06-14 14:40:19 -0400

    working on editing posts

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @post.comments.create(comment_params)
+    redirect_to @post, notice: "Comment successful."
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to @post, notice: "Comment deleted."
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:author, :body)
+  end
+end

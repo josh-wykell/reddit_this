47dbcc713420922e29adc95545cce88ef13b3ef2
Derek Dyer <derek@digitalderek.com>
2015-06-14 11:02:57 -0400

    added placeholder for votes and cleaned up post.erb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612191107) do
+ActiveRecord::Schema.define(version: 20150614120318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150612191107) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "author"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
